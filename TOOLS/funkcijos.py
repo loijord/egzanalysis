c8a00127eee19dc08a77932cc26d26978af11249
Simonas Mamaitis <simonas.mamaitis@gmail.com>
2019-11-21 01:36:44 +0200

    update: funkcijų braižyklėje atsirado galimybė brėžti eskizą pagal jau pridėtus taškus

--- TOOLS/funkcijos.py
+++ TOOLS/funkcijos.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import sympy as sp
+from scipy.interpolate import interp1d
 
 class FunctionPlotter:
     def __init__(self, expression, xlim = (-10,10), ylim = (-10,10), step=1):
@@ -11,7 +12,7 @@ class FunctionPlotter:
         self.ylim = ylim
         self.step = step
         self.input = ''
-        self.arguments = []
+        self.points = []
 
     def display(self):
         title = str(self.expression)
@@ -23,26 +24,48 @@ class FunctionPlotter:
         ax.spines['bottom'].set_position('center')
         ax.spines['right'].set_color('none')
         ax.spines['top'].set_color('none')
-        #ax.xaxis.set_ticks_position('bottom')
-        #ax.yaxis.set_ticks_position('left')
+        ax.xaxis.set_ticks_position('bottom')
+        ax.yaxis.set_ticks_position('left')
         plt.xticks(np.arange(self.xlim[0], self.xlim[1], step=self.step))
         plt.yticks(np.arange(self.ylim[0], self.ylim[1], step=self.step))
 
         fig.canvas.mpl_connect("key_press_event", lambda event: self.refresh_plot(fig, ax, event.key))
         plt.xlim(*self.xlim)
         plt.ylim(*self.ylim)
-
+        plt.grid()
         plt.show()
 
     def refresh_plot(self, fig, ax, key):
         if key == 'enter':
-            try: argument = float(self.input.replace(',','.'))
-            except ValueError('wrong input'): self.input = ''
-            ax.scatter(argument, self.function(argument), color='blue')
-            fig.canvas.draw()
+            try:
+                argument = float(self.input.replace(',', '.'))
+                format_is_right = True
+            except ValueError('not a float'):
+                format_is_right = False
+            if format_is_right:
+                self.points.append([argument, self.function(argument)])
+                ax.scatter(*self.points[-1], color='blue')
+                fig.canvas.draw()
             self.input = ''
+        elif key == 'up':
+            self.sketch(fig, ax)
         else:
             self.input += key
 
+    def sketch(self, fig, ax):
+        #connects points choosen to interpolate
+        if len(self.points) > 1:
+            x, y = np.array(self.points).T
+            if len(self.points) > 3: f = interp1d(x, y, kind='cubic', fill_value="extrapolate")
+            elif len(self.points) > 2: f = interp1d(x, y, kind='quadratic', fill_value="extrapolate")
+            elif len(self.points) > 1: f = interp1d(x, y, kind='linear', fill_value="extrapolate")
+
+            arguments = np.arange(min(x)-self.step, max(x)+self.step, self.step/100)
+            values = f(arguments)
+            ax.plot(arguments, values)
+            fig.canvas.draw()
+
+
 functionplotter = FunctionPlotter('sin(x)', xlim = (-10,10), ylim = (-10,10), step=1)
 functionplotter.display()
+
